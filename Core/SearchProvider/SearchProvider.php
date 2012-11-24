cb4dc3b32307dcb5fa0ec091975beb9c0753169d
alphalemon <user@server.com>
2012-11-24 11:57:53 +0100

    required page name instead of route which is generated using locale

--- Core/SearchProvider/SearchProvider.php
+++ Core/SearchProvider/SearchProvider.php
@@ -32,12 +32,12 @@ class SearchProvider implements ProviderInterface
     public function populate(\Closure $loggerClosure = null)
     {
         if ($loggerClosure) {
-            $loggerClosure('Indexing users');
+            $loggerClosure('Indexing website');
         }
         
         $deployBundle = new AlAsset($this->kernel, $this->deployBundle);
         $deployBundlePath = $deployBundle->getRealPath();
-                
+        
         $i = 0;
         $documents = array();      
         $routesPath = $deployBundlePath . '/Resources/config/site_routing.yml';
@@ -45,7 +45,6 @@ class SearchProvider implements ProviderInterface
         $routes = array_keys($routes);
         foreach ($routes as $route) {
             $link = $this->router->generate($route);
-            
             
             $client = new Client($this->kernel);
             $client->request('GET', $link);            
@@ -62,36 +61,7 @@ class SearchProvider implements ProviderInterface
             
             $i++;
         }
-        /*
-        $templatesPath = $deployBundlePath . '/Resources/search_data';
-        $finder = new Finder();
-        $templates = $finder->files()->depth(0)->in($templatesPath);
-        $i = 0;
-        foreach ($templates as $template) {
-            $file = (string)$template;
-            $fileName = basename($file, '.html.twig');
-            
-            $route = $this->router->generate($fileName);
-            $client = new \Symfony\Component\HttpKernel\Client($this->kernel);
-            $client->request('GET', $route);
-            
-            $content = (string)$client->getCrawler()->text();
-            $content = preg_replace('/\/\*\<\!\[CDATA\[\*\/(.*?)\/\*\]\]\>\*\//s', '', $content);
-            $content = strip_tags($content);
-            
-            $page = array(
-                //"contents" => implode(" ", array_map(function($value){ return strip_tags($value); }, $matches[1])),
-                "contents" => $content,
-                "url" => $this->router->generate($fileName),
-            );
-            
-            $document = new \Elastica_Document($i);
-            $document->setData($page);
-            $documents[] = $document;
-            
-            $i++;
-        }
-        */
+        
         try {
             $this->userType->addDocuments($documents);
         }
